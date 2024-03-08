95bd0d6e2d2b88ef491aafb0962f0f9ee97a49e2
Asem130 <97855100+Asem130@users.noreply.github.com>
2024-03-08 14:12:48 +0200

    add main list view

--- lib/Features/home/presentaion/views/widgets/home_view_body.dart
+++ lib/Features/home/presentaion/views/widgets/home_view_body.dart
@@ -1,8 +1,8 @@
-
 import 'package:bookly/Features/home/presentaion/views/widgets/app_bar.dart';
 import 'package:bookly/Features/home/presentaion/views/widgets/main_list_view_item.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -12,8 +12,24 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
-        MainListViewItem(),
+        MainListView(),
       ],
+    );
+  }
+}
+
+class MainListView extends StatelessWidget {
+  const MainListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.25,
+      child: ListView.builder(
+          itemCount: 5,
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          itemBuilder: ((context, index) => const MainListViewItem())),
     );
   }
 }
--- lib/Features/home/presentaion/views/widgets/main_list_view_item.dart
+++ lib/Features/home/presentaion/views/widgets/main_list_view_item.dart
@@ -6,13 +6,12 @@ class MainListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.25,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
       child: AspectRatio(
         aspectRatio: 2.7 / 4,
         child: Container(
           decoration: BoxDecoration(
-           
             borderRadius: BorderRadius.circular(16),
             image: const DecorationImage(
                 fit: BoxFit.cover, image: AssetImage(AssetesData.testImage)),
