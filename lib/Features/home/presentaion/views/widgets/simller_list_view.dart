47e15a850a90cbf56f88ceec9d99d47f28c77ffc
Asem130 <97855100+Asem130@users.noreply.github.com>
2024-03-12 04:01:16 +0200

    search view done & create book model

--- lib/Features/home/presentaion/views/widgets/simller_list_view.dart
+++ lib/Features/home/presentaion/views/widgets/simller_list_view.dart
@@ -1,23 +1,23 @@
 import 'package:bookly/Features/home/presentaion/views/widgets/main_list_view_item.dart';
 import 'package:flutter/material.dart';
 
-class MainListView extends StatelessWidget {
-  const MainListView({super.key});
+class SimllerListView extends StatelessWidget {
+  const SimllerListView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.25,
+      height: MediaQuery.of(context).size.height * 0.13,
       child: ListView.builder(
         padding: EdgeInsets.zero,
         itemCount: 10,
         physics: const BouncingScrollPhysics(),
         scrollDirection: Axis.horizontal,
-        itemBuilder: ((context, index) => const Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: 8,
+        itemBuilder: ((context, index) => Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: MainListViewItem(
+                borderRadius: BorderRadius.circular(8),
               ),
-              child: MainListViewItem(),
             )),
       ),
     );
